6b0f0ff4911e3f9fd2ac641a85c1e69ca5aaf21b
Ashutosh Salve <ashutosh.salve@13gmail.com>
2023-03-21 11:44:20 +0530

    first commit

---
+++ Revs.java
@@ -0,0 +1,14 @@
+class Revs
+{
+public static void main(String[] args)
+{
+String A = "ashutosH";
+String rev = "";
+int a;
+for(a = A.length() - 1; a >= 0; a--)
+{
+rev = rev + A.charAt(a);
+}
+System.out.println(rev);
+}
+}
---
+++ Rrrrra.java
@@ -0,0 +1,19 @@
+class Rrrrra
+{
+int a;
+int b;
+Rrrrra(int a , int b)
+{
+this.a = a;
+this.b = b;
+}
+void play()
+{
+System.out.println("done");
+}
+public static void main(String[] args)
+{
+Rrrrra G = new Rrrrra(10 , 20);
+G.play();
+}
+}
